e5ada653cb6e98ffd1ede1cfe19dcfc7e1c8a44d
zhouxin <tastttttty@gmail.com>
2022-07-07 20:33:05 +0800

    2

---
+++ h2/app/src/main/java/com/homework2/message.kt
@@ -0,0 +1,20 @@
+package com.homework2
+
+import android.os.Bundle
+//import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+
+class message : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_message, container, false)
+    }
+
+}
