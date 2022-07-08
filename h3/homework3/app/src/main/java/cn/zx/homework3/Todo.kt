4f07587974c8301186538c40edc46b861adc0ac0
zhouxin <tastttttty@gmail.com>
2022-07-08 10:11:06 +0800

    3

---
+++ h3/homework3/app/src/main/java/cn/zx/homework3/Todo.kt
@@ -0,0 +1,17 @@
+package cn.zx.homework3
+
+
+data class Todo(
+    val content: String,
+    val createTime: Long,
+) {
+    companion object {
+        val TABLE = "Todo"
+        val COL_ID = "id"
+        val COL_CONTENT = "content"
+        val COL_TIME = "createTime"
+    }
+
+    var id: Int? = null
+
+}
